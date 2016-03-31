3fceef256d43b9f715d083654448454a658bfc37
denil1111 <zyh.work@gmail.com>
2016-03-31 10:10:53 +0800

    put data in client

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 var koa = require('koa');
 var app = koa();
+var route = require('koa-route');
+var render = require("./lib/render")
 
 // logger
 
@@ -12,12 +14,24 @@ app.use(function*(next) {
 
 // response
 
-app.use(function*() {
+app.use(route.get('/randomrun', function*() {
 	var addon = require('../../build/Release/bike');
-	console.log("haha");
-	console.log(addon.runMerge(10)); 
-
-	this.body = 'Hello World';
-});
+	var datas = addon.randomData(10); 
+	var path = addon.runMerge(10); 
+	console.log(datas);
+	console.log(path);
+	var point = [];
+	var demand = [];
+	// datas.forEach(function(data){
+ //  		point.push({x:data.x,y:data.y});
+ //  		demand.push(data.demand);
+	// }); 
+	for (data of yield datas) {
+		point.push({x:data.x,y:data.y});
+  		demand.push(data.demand);
+	}
+	this.body = yield render("index",{demand:demand,point:point,path:path});
+}));
 
 app.listen(3000);
+console.log('listening on port 3000');
